8629e46943669bebaf3d085e97021b73cacad889
Igor Cherkashov <cherkashov270999@gmail.com>
2023-11-17 23:02:57 +0300

    Fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -31,7 +31,7 @@ def main():
                     correct_answer = 'no'
             print(f'{answer} is wrong answer;(. Correct answer was {correct_answer}.')
             print(f"Let's try again, {name}!")
-            correct_answers = 1
+            correct_answers = 0
     print(f"Congratulations, {name}!")
 
 
