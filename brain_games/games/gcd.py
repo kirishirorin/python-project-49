600d8b9d2f434ce11c1adb4714ab17ebc387a449
Igor Cherkashov <cherkashov270999@gmail.com>
2024-01-28 14:25:22 +0300

    add gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+QUESTION = 'Find the greatest common divisor of given numbers.'
+
+def game_round():
+    rand_num_1 = randint(1, 100)
+    rand_num_2 = randint(1, 100)
+    expression = f'Question: {rand_num_1} {rand_num_2}'
+    for i in range(max(rand_num_1, rand_num_2), 0, -1):
+        if (rand_num_1 % i == 0) and (rand_num_2 % i == 0):
+            correct_answer = str(i)
+            break
+    return expression, correct_answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,34 +1,10 @@
 #!/usr/bin/env python3
-import prompt
-from random import randint
+from brain_games.games import gcd
+from brain_games import engine
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
-    correct_answers = 0
-    while correct_answers < 3:
-        rand_num_1 = randint(1, 100)
-        rand_num_2 = randint(1, 100)
-        print(f'Question: {rand_num_1} {rand_num_2}')
-        answer = prompt.integer('Your answer: ')
-        correct_answer = 0
-        for i in range(max(rand_num_1, rand_num_2), 0, -1):
-            if (rand_num_1 % i == 0) and (rand_num_2 % i == 0):
-                correct_answer = i
-                break
-        if answer == correct_answer:
-            print("Correct!")
-            correct_answers += 1
-        else:
-            print(f'{str(answer)} is wrong answer ;(.'
-                  f' Correct answer was {str(correct_answer)}.')
-            print(f"Let's try again, {name}!")
-            break
-    if correct_answers == 3:
-        print(f"Congratulations, {name}!")
+    engine.run(gcd)
 
 
 if __name__ == '__main__':
