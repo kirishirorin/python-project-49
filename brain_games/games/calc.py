6a7dee66fdc40f27b60cc296bc89f6c03194159f
Igor Cherkashov <cherkashov270999@gmail.com>
2024-02-14 00:13:11 +0300

    fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,13 +1,13 @@
-from random import choice
+import random
 
 
 QUESTION = 'What is the result of the expression?'
 
 
 def game_round():
-    rand_num_1 = randint(1, 100)
-    rand_num_2 = randint(1, 100)
-    operator = choice(['+', '-', '*'])
-    expression = f'{num1} {operator} {num2}'
-    correct_answer = correct_answer = str(eval(expression))
+    rand_num_1 = random.randint(1, 100)
+    rand_num_2 = random.randint(1, 100)
+    operator = random.choice(['+', '-', '*'])
+    expression = f'{rand_num_1} {operator} {rand_num_2}'
+    correct_answer = str(eval(expression))
     return expression, str(correct_answer)
