1f73a93218c5012e78bce90ea19be6b1f0b35d2a
Igor Cherkashov <cherkashov270999@gmail.com>
2024-02-19 20:31:32 +0300

    fix brain-calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,6 +8,6 @@ def game_round():
     rand_num_1 = random.randint(1, 100)
     rand_num_2 = random.randint(1, 100)
     operator = random.choice(['+', '-', '*'])
-    expression = f'{rand_num_1} {operator} {rand_num_2}'
-    correct_answer = str(eval(expression))
+    expression = f'Question: {rand_num_1} {operator} {rand_num_2}'
+    correct_answer = str(eval(f'{rand_num_1} {operator} {rand_num_2}'))
     return expression, str(correct_answer)
