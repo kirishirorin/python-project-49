baeec085986fe49655b122686e2d76d8b0f5401d
Igor Cherkashov <cherkashov270999@gmail.com>
2024-07-16 20:26:29 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,7 +7,7 @@ END_NUM = 100
 
 
 def is_prime(rand_num_1):
-    for i in range(2, rand_num_1 + 1):
+    for i in range(2, rand_num_1):
         if rand_num_1 % i == 0:
             return False
     return True
